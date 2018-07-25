d3e32c2ec62d1fb19c8e4f6f5a4993dfb7677ffb
Erin Hogan <hogan10@charter.net>
2018-07-24 22:06:25 -0500

    decisions updated

---
+++ Decisions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+                message = "You won a new car!";
+            else if (userValue == "2")
+                message = "You won a new boat!";
+            else if (userValue == "3")
+                message = "You won a new cat!";
+            else
+            //if you have more than one message statement you must use curly brackets on either side. If it is only one, you don't need them.
+            {
+                message = "Sorry, we didn't understand. ";
+                //could also do message += "You lose.";
+                message = message + "You lose";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+            //Console.WriteLine("You won a {0}.", message);
+            //0 puts in what they entered, 1 puts in what they should win
+            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
+            Console.ReadLine();
+        }
+    }
+}
